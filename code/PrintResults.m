c9b609ab41571e697faec7c6d00bfefe4ded7762
Sverre Velten Rothmund <sverrevr@ntnu.no>
2016-04-29 14:42:52 +0200

    La til gjennomsnittplott matlab

--- code/PrintResults.m
+++ code/PrintResults.m
@@ -1,13 +1,24 @@
 clear;
-errorRateTesting = importdata('errorRateTesting.txt');
-errorRateTraining = importdata('errorRateTraining.txt');
-errorRateTraining = [errorRateTraining(1);errorRateTraining];
+runs = 5;
+values = 26;
+errorRateTesting = zeros(1,values);
+errorRateTraining = zeros(1,values);
+for i=[1:runs]
+    errorRateTestingTemp = (importdata(strcat('Results/Run',int2str(i),'/errorRateTesting.txt')))';
+    errorRateTrainingTemp = (importdata(strcat('Results/Run',int2str(i),'/errorRateTraining.txt')))';
+    errorRateTrainingTemp = [errorRateTrainingTemp(1),errorRateTrainingTemp];
+    errorRateTesting = errorRateTesting + errorRateTestingTemp;
+    errorRateTraining = errorRateTraining + errorRateTrainingTemp;
+end
+
+errorRateTesting = errorRateTesting ./ runs;
+errorRateTraining = errorRateTraining ./ runs;
 
 hold on
 plot(0:(length(errorRateTesting)-1),errorRateTesting);
 plot(0:(length(errorRateTesting)-1),errorRateTraining);
 legend('Testing','Training');
-title('Run5');
+title(strcat('Average of ',32,int2str(runs), ' runs'));
 xlabel('Itteration');
 ylabel('Error rate');
 if(errorRateTesting(1) > errorRateTraining(1))
